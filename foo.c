c95b47721cc710951f49169925e7a66708485459
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-18 10:53:14 -0800

    Replace printf with write().

--- foo.c
+++ foo.c
@@ -3,5 +3,12 @@
 
 int main(void)
 {
-    printf("Hello, world!\n");
+    char *buf = "Hello, world!\n";
+    write(1, buf, 14);
+
+    // first argument is file descriptor:
+    // 3 are available out of the box:
+    // 0 - standard input
+    // 1 - standard output (screen)
+    // 2 - error
 }
