a6a0704e9daa8d6bfd882145702147a9ce690384
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-18 11:47:16 -0800

    Another confusing example.

--- foo.c
+++ foo.c
@@ -1,20 +1,14 @@
 #include <stdio.h>  // printf
 #include <unistd.h> // open, close, read, write
 
+int addOne(int *x) // x = b
+{
+    *x = *x + 1;
+}
+
 int main(void)
 {
-    char c = 'B'; // character
-    char *p;      // character pointer
-
-    p = &c; // & address-of operator
-            // this crates a pointer to a char
-
-    printf("%c\n", c);
-    printf("%s\n", p); // this displays weird because there's no null termination
-
-    // printf example, substituting
-    // printf("%c %d %s", 'c', 123, "hi there!");
-
-    // dereference a pointer with a deref operator
-    printf("%c\n", *p);
+    int b = 12;
+    addOne(&b);
+    printf("%d\n", b);
 }
